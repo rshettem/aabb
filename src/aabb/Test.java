1315f73190c664804319002f4f278a47bc8f78ea
sathya <sathya@sathya-PC>
2018-11-30 11:49:35 +0530

    WH-432 threee

--- src/aabb/Test.java
+++ src/aabb/Test.java
@@ -5,5 +5,36 @@ public class Test {
 	private int empId;
 	private String empName;
 	
+	private String empDept;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getEmpDept() {
+		return empDept;
+	}
+
+	public void setEmpDept(String empDept) {
+		this.empDept = empDept;
+	}
+
+	@Override
+	public String toString() {
+		return "Test [empId=" + empId + ", empName=" + empName + ", empDept=" + empDept + "]";
+	}
+	
 	
 }
